6ca91e9f5d492448cdc8d32b34e9e77c79945257
Luiz Parreira <luizfilipester@gmail.com>
2017-06-18 17:03:07 -0300

    Implemented assets module and its tests
    Still missing testing passing params in

---
+++ lib/kraken_ex/assets.ex
@@ -0,0 +1,17 @@
+defmodule KrakenEx.Assets do
+  alias KrakenEx.PublicClient
+
+  def assets(method \\ "Assets") do
+    method
+    |> PublicClient.get
+    |> _parse_response
+  end
+
+  defp _parse_response({:ok, response}), do: _parse_body(response.body)
+  defp _parse_response(other_response), do: other_response
+
+  defp _parse_body(%{"error" => [], "result" => result}), do: {:ok, result}
+  defp _parse_body(%{"error" => errors, "result" => result}) do
+    {:error, errors}
+  end
+end
