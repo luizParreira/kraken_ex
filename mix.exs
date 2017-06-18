923994883a0fa3c37be17d9cd0ed9bea224f88b5
Luiz Parreira <luizfilipester@gmail.com>
2017-06-18 14:44:05 -0300

    Added dependencies

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule KrakenEx.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +28,11 @@ defmodule KrakenEx.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:poison, "~> 3.1"},
+      {:httpoison, "~> 0.11.1"},
+      {:exvcr, "~> 0.8", only: :test},
+      {:ex_doc, "~> 0.15.1", only: :dev}
+    ]
   end
 end
