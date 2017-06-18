5ca310fa0d52c2b088fa37129e74388a1b36d14d
Luiz Parreira <luizfilipester@gmail.com>
2017-06-18 18:35:29 -0300

    Updated support for elixir 1.3 as well

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KrakenEx.Mixfile do
   def project do
     [app: :kraken_ex,
      version: "0.1.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
