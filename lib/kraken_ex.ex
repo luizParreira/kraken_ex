4d18e8ac27a4135855e78a7cff54e53e5c7720bb
Luiz Parreira <luizfilipester@gmail.com>
2017-06-18 12:54:56 -0300

    Initial commit

---
+++ lib/kraken_ex.ex
@@ -0,0 +1,18 @@
+defmodule KrakenEx do
+  @moduledoc """
+  Documentation for KrakenEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KrakenEx.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
