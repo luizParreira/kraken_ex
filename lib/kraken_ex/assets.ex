ca5c76e99fb9d1ebc311df586001e75ca7824dee
Luiz Parreira <luizfilipester@gmail.com>
2017-06-19 21:20:34 -0300

    Fixed unnecessaru calls to `_` functions

--- lib/kraken_ex/assets.ex
+++ lib/kraken_ex/assets.ex
@@ -4,14 +4,14 @@ defmodule KrakenEx.Assets do
   def assets(method \\ "Assets") do
     method
     |> PublicClient.get
-    |> _parse_response
+    |> parse_response
   end
 
-  defp _parse_response({:ok, response}), do: _parse_body(response.body)
-  defp _parse_response(other_response), do: other_response
+  defp parse_response({:ok, response}), do: parse_body(response.body)
+  defp parse_response(other_response), do: other_response
 
-  defp _parse_body(%{"error" => [], "result" => result}), do: {:ok, result}
-  defp _parse_body(%{"error" => errors}) do
+  defp parse_body(%{"error" => [], "result" => result}), do: {:ok, result}
+  defp parse_body(%{"error" => errors}) do
     {:error, errors}
   end
 end
