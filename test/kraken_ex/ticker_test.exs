a056b25e59a91a58cb6c1d2a18e12891da270a67
Luiz Parreira <luizfilipester@gmail.com>
2017-06-20 22:38:42 -0300

    Added docs and refactored ticker module

--- test/kraken_ex/ticker_test.exs
+++ test/kraken_ex/ticker_test.exs
@@ -2,7 +2,10 @@ defmodule KrakenEx.TickerTest do
   use ExUnit.Case, async: false
   use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
-  alias KrakenEx.PublicClient
+  alias KrakenEx.{
+    PublicClient,
+    PairRequiredParamError
+  }
 
   setup_all do
     PublicClient.start
@@ -12,10 +15,16 @@ defmodule KrakenEx.TickerTest do
 
   test "get Tickers for given pairs available" do
     use_cassette "get_ticker_based_on_pairs" do
-      {:ok, asset_pairs} = KrakenEx.ticker(pairs: @pairs)
+      {:ok, asset_pairs} = KrakenEx.ticker(pair: @pairs)
       asset_pairs_names = Map.keys(asset_pairs)
       assert Enum.sort(asset_pairs_names) == Enum.sort(@pairs)
       assert asset_pairs["XXBTZJPY"] |> Map.keys |> Enum.sort == ~w(a b c v p t l h o) |> Enum.sort
+    end
+  end
+
+  test "get ticker data without informing `pair:`" do
+    assert_raise PairRequiredParamError, fn ->
+      KrakenEx.ticker(inteval: 10)
     end
   end
 end
